51ade79355c5e6a54ab3e778fa60e4cbab591986
modijani <mohammadkhan.mail@gmail.com>
2023-02-27 17:59:58 +0100

    Camera smoothed

--- Assets/Camera_Controller.cs
+++ Assets/Camera_Controller.cs
@@ -6,7 +6,10 @@ public class Camera_Controller : MonoBehaviour
 {
     public Transform playerFollow;
     public Transform playerFollowSecond;
-    private float playerPositionToFollow;
+    public float offset;
+    public float offsetSmoothing;
+    private Transform playerPositionToFollow;
+    private Vector3 positionForCameraTofollow;
 
     // Start is called before the first frame update
     void Start()
@@ -16,15 +19,44 @@ public class Camera_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      
+    }
+
+    void LateUpdate()
+    {
         followLowerPlayer();
-        Vector3 pos = transform.position;
-        pos.y = playerPositionToFollow;
-        transform.position = pos;
+
+        positionForCameraTofollow = new Vector3(transform.position.x,
+            playerPositionToFollow.position.y, transform.position.z);
+
+        if (playerPositionToFollow.localScale.y > 0f)
+        {
+            positionForCameraTofollow = new
+                Vector3(positionForCameraTofollow.x,
+                positionForCameraTofollow.y + offset,
+                positionForCameraTofollow.z);
+        }
+        else
+        {
+            positionForCameraTofollow = new
+                    Vector3(positionForCameraTofollow.x,
+                positionForCameraTofollow.y - offset,
+                positionForCameraTofollow.z);
+        }
+
+        transform.position = Vector3.Lerp(transform.position,
+            positionForCameraTofollow, offsetSmoothing * Time.deltaTime);
     }
 
     void followLowerPlayer()
     {
-        playerPositionToFollow = Mathf.Min(playerFollow.position.y,
-            playerFollowSecond.position.y);
+        if (playerFollow.position.y < playerFollowSecond.position.y)
+        {
+            playerPositionToFollow = playerFollow;
+        } else
+        {
+            playerPositionToFollow = playerFollowSecond;
+        }
+        
     }
 }
