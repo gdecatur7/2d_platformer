6dedf4d8ea3270f4730df6709cac7bdd979fe498
modijani <mohammadkhan.mail@gmail.com>
2023-02-27 17:08:06 +0100

    Done

---
+++ Assets/Scripts/Doorway.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Doorway : MonoBehaviour
+{
+    public bool isPlayerHere;
+
+    void Start()
+    {
+        isPlayerHere = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Player")
+        {
+            isPlayerHere = true;
+            collision.gameObject.SetActive(false);
+        }
+
+        Physics2D.IgnoreCollision(collision.collider, collision.otherCollider);
+    }
+}
